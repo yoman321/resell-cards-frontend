858198a08d3124b2a4d4594f53fcf41d1c413960
yoman321 <luophilipe@gmail.com>
2024-05-25 22:21:49 -0400

    fixed filtered row model

--- src/components/pages/Inventory.tsx
+++ src/components/pages/Inventory.tsx
@@ -241,8 +241,8 @@ const Inventory = () => {
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
+      <div className="flex items-center justify-between space-x-2 py-4">
+        <div className="text-sm text-muted-foreground">
           {table.getFilteredSelectedRowModel().rows.length} of{" "}
           {table.getFilteredRowModel().rows.length} row(s) selected.
         </div>
