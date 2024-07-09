1784b94604615daf82e9b8c611caa0a11609256e
yoman321 <luophilipe@gmail.com>
2024-07-08 20:40:01 -0400

    added tests to verify if fetchMtgInventory is being called

--- __tests__/Inventory.test.tsx
+++ __tests__/Inventory.test.tsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import { cleanup, fireEvent, render, screen, renderHook } from '@testing-library/react';
+import { vi } from "vitest";
 
 import Inventory from '../src/pages/Inventory.tsx';
 import { fetchMtgInventory } from '../src/features/mtg-inventory-feature/hooks/MtgInventoryHooks.tsx'
 import { initialMtgInventoryStoreState } from '../src/features/mtg-inventory-feature/stores/MtgInventoryStore.tsx';
 
 
-afterEach(cleanup);
+describe('testing the fetchMtgInventory function', () => {
+  const fetchMtgInventorySpy = vi.fn(() => fetchMtgInventory);
 
-it('should render intial/dummy inventory data for the inventory page with ', () => {
-  const inventoryData = renderHook(() => fetchMtgInventory());
-  expect(inventoryData).toBe(initialMtgInventoryStoreState);
+  test('should call fetchMtgInventory', () => {
+
+    fetchMtgInventorySpy();
+
+    expect(fetchMtgInventorySpy).toHaveBeenCalled();
+  })
+
+  test('should return dummy values from fetchMtgInventory', () => {
+
+    fetchMtgInventorySpy.mockReturnValueOnce(() => initialMtgInventoryStoreState);
+    fetchMtgInventorySpy();
+
+    expect(fetchMtgInventorySpy).toHaveReturnedWith(initialMtgInventoryStoreState);
+  })
+
 })
-//it('should make add card dialog appear on Add Card button click', () => {
-// render(<Inventory />);
+//it('should render intial/dummy inventory data for the inventory page with ', () => {
+//  const inventoryData = renderHook(() => fetchMtgInventory());
+//  expect(inventoryData).toBe(initialMtgInventoryStoreState);
+//})
+//it('should make add card dialog appear on Add Card button click',  () => {
+//  render(<Inventory />);
 //
-// const addCardButton = screen.getByRole('button', {
-//   name: 'Add Card'
-// });
-// const dialogSubText = 'Add a card to your inventory';
+//  const addCardButton = screen.getByRole('button', {
+//    name: 'Add Card'
+//  });
+//  const dialogSubText = 'Add a card to your inventory';
 //
 //  expect(screen.getByText(dialogSubText)).toBeFalsy();
 //  fireEvent.click(addCardButton);
-// expect(screen.getByText(dialogSubText)).toBeTruthy();
+//  expect(screen.getByText(dialogSubText)).toBeTruthy();
 //
 //})
