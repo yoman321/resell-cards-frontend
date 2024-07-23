9be47c2437a44f7d6d6d5e29d5de51f2caf56e9a
yoman321 <luophilipe@gmail.com>
2024-07-23 19:11:50 -0400

    finished tests for Inventory page

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './__tests__/Setup.ts'
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
